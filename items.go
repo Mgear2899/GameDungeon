3d1cfdb52102c2a8b05457640405419008211f08
o.zuev <volgin2899@gmail.com>
2024-10-21 16:55:11 +0300

    Initial commit

---
+++ items.go
@@ -0,0 +1,19 @@
+package main
+
+var items = map[string]Items{
+	"1": {
+		Name:  "Ср. зелье лечения",
+		Param: 17,
+		Count: 0,
+	},
+	"2": {
+		Name:  "Бол. зелье лечения",
+		Param: 23,
+		Count: 0,
+	},
+	"3": {
+		Name:  "Мал. зелье лечения",
+		Param: 10,
+		Count: 0,
+	},
+}
